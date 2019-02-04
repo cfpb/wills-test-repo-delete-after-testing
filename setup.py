83bb7e8661c3b81dbfc006a59cd0190c2cfaebed
Will Barton <william.barton@cfpb.gov>
2019-02-04 15:46:49 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='wills-great-testpkg',
+    url='https://github.com/cfpb/wills-test-repo-delete-after-testing',
+    author='CFPB',
+    author_email='tech@cfpb.gov',
+    description='',
+    long_description='',
+    long_description_content_type='text/markdown',
+    license='CC0',
+    version='1.0.0',
+    packages=find_packages(),
+)
